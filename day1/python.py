d930ae9e44d264dcf72a5e85efed7160a02e6c44
gizu <zuraba000@gmail.com>
2022-09-19 21:18:35 +0400

    first commit

---
+++ day1/python.py
@@ -0,0 +1,28 @@
+from sunau import AUDIO_FILE_ENCODING_LINEAR_32
+
+
+name = " my name is"
+name2 = "giorgi."
+age = 20
+height = 176.5
+knows_programing = "Not well, but I'm learning now"
+surname = " my surname is zurabashvili,"
+nickname = " And my nickname is also zuraba."
+age2 = "i am "
+age3 = "years old!"
+height = "aand my height is "
+a = "Also"
+ab = "Do I know programming?"
+
+
+
+print(name + " " + name2 + " " + surname  + " " + nickname + " " + a + " " + age2 + " " + str(age) + " " + age3 + " " + ab + " " + knows_programing)
+
+
+
+
+
+
+
+
+
